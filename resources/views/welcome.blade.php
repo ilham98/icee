c0b88f6531978c921dc4a13950ddc49c10bfd1b7
Gunawan101 <g16615002@gmail.com>
2019-06-19 15:23:04 +0800

    mantap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mantap jivva
                 </div>
 
                 <div class="links">
